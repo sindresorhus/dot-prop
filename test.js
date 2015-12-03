6a89c6a20a7371f7a58dc2e1713af73ab9b51317
Steve Mao <maochenyan@gmail.com>
2015-12-03 23:31:17 +1100

    housekeeping

--- test.js
+++ test.js
@@ -1,78 +1,74 @@
-'use strict';
-var test = require('ava');
-var dotProp = require('./');
+import test from 'ava';
+import dotProp from './';
 
 test(function getter(t) {
-	var f1 = {foo: {bar: 1}};
-	t.assert(dotProp.get(f1) === f1);
-	t.assert(dotProp.get(f1, 'foo') === f1.foo);
-	t.assert(dotProp.get({foo: 1}, 'foo') === 1);
-	t.assert(dotProp.get({foo: null}, 'foo') === null);
-	t.assert(dotProp.get({foo: undefined}, 'foo') === undefined);
-	t.assert(dotProp.get({foo: {bar: true}}, 'foo.bar') === true);
-	t.assert(dotProp.get({foo: {bar: {baz: true}}}, 'foo.bar.baz') === true);
-	t.assert(dotProp.get({foo: {bar: {baz: null}}}, 'foo.bar.baz') === null);
-	t.assert(dotProp.get({foo: {bar: 'a'}}, 'foo.fake.fake2') === undefined);
+	let f1 = {foo: {bar: 1}};
+	t.is(dotProp.get(f1), f1);
+	t.is(dotProp.get(f1, 'foo'), f1.foo);
+	t.is(dotProp.get({foo: 1}, 'foo'), 1);
+	t.is(dotProp.get({foo: null}, 'foo'), null);
+	t.is(dotProp.get({foo: undefined}, 'foo'), undefined);
+	t.is(dotProp.get({foo: {bar: true}}, 'foo.bar'), true);
+	t.is(dotProp.get({foo: {bar: {baz: true}}}, 'foo.bar.baz'), true);
+	t.is(dotProp.get({foo: {bar: {baz: null}}}, 'foo.bar.baz'), null);
+	t.is(dotProp.get({foo: {bar: 'a'}}, 'foo.fake.fake2'), undefined);
 
-	function fn () {}
+	function fn() {}
 	fn.foo = {bar: 1};
-	t.assert(dotProp.get(fn), fn)
-	t.assert(dotProp.get(fn, 'foo') === fn.foo)
-	t.assert(dotProp.get(fn, 'foo.bar') === 1);
+	t.is(dotProp.get(fn), fn);
+	t.is(dotProp.get(fn, 'foo'), fn.foo);
+	t.is(dotProp.get(fn, 'foo.bar'), 1);
 
-	t.assert(dotProp.get({'foo.baz': {bar: true}}, 'foo\\.baz.bar') === true);
-	t.assert(dotProp.get({'fo.ob.az': {bar: true}}, 'fo\\.ob\\.az.bar') === true);
-	t.end();
+	t.is(dotProp.get({'foo.baz': {bar: true}}, 'foo\\.baz.bar'), true);
+	t.is(dotProp.get({'fo.ob.az': {bar: true}}, 'fo\\.ob\\.az.bar'), true);
 });
 
 test(function setter(t) {
-	var f1 = {};
+	let f1 = {};
 
 	function func() {
 		return 'test';
 	}
 
 	dotProp.set(f1, 'foo', 2);
-	t.assert(f1.foo === 2);
+	t.is(f1.foo, 2);
 
 	f1 = {foo: {bar: 1}};
 	dotProp.set(f1, 'foo.bar', 2);
-	t.assert(f1.foo.bar === 2);
+	t.is(f1.foo.bar, 2);
 
 	dotProp.set(f1, 'foo.bar.baz', 3);
-	t.assert(f1.foo.bar.baz === 3);
+	t.is(f1.foo.bar.baz, 3);
 
 	dotProp.set(f1, 'foo.bar', 'test');
-	t.assert(f1.foo.bar === 'test');
+	t.is(f1.foo.bar, 'test');
 
 	dotProp.set(f1, 'foo.bar', null);
-	t.assert(f1.foo.bar === null);
+	t.is(f1.foo.bar, null);
 
 	dotProp.set(f1, 'foo.bar', false);
-	t.assert(f1.foo.bar === false);
+	t.is(f1.foo.bar, false);
 
 	dotProp.set(f1, 'foo.bar', undefined);
-	t.assert(f1.foo.bar === undefined);
+	t.is(f1.foo.bar, undefined);
 
 	dotProp.set(f1, 'foo.fake.fake2', 'fake');
-	t.assert(f1.foo.fake.fake2 === 'fake');
+	t.is(f1.foo.fake.fake2, 'fake');
 
 	dotProp.set(f1, 'foo.function', func);
-	t.assert(f1.foo.function === func);
+	t.is(f1.foo.function, func);
 
-	function fn () {}
+	function fn() {}
 	dotProp.set(fn, 'foo.bar', 1);
-	t.assert(fn.foo.bar === 1);
+	t.is(fn.foo.bar, 1);
 
 	f1.fn = fn;
 	dotProp.set(f1, 'fn.bar.baz', 2);
-	t.assert(f1.fn.bar.baz, 2);
+	t.is(f1.fn.bar.baz, 2);
 
 	dotProp.set(f1, 'foo\\.bar.baz', true);
-	t.assert(f1['foo.bar'].baz === true);
+	t.is(f1['foo.bar'].baz, true);
 
 	dotProp.set(f1, 'fo\\.ob\\.ar.baz', true);
-	t.assert(f1['fo.ob.ar'].baz === true);
-
-	t.end();
+	t.is(f1['fo.ob.ar'].baz, true);
 });
