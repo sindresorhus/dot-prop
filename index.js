159181956452f7c6f679f56645a00937af92d264
Steve Mao <maochenyan@gmail.com>
2016-07-16 19:18:32 +1000

    add null check for `has` on the path that's not the last bit
    fixes #28

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ module.exports.has = function (obj, path) {
 	for (var i = 0; i < pathArr.length; i++) {
 		obj = obj[pathArr[i]];
 
-		if (obj === undefined) {
+		if (obj === undefined || (i !== pathArr.length - 1 && obj === null)) {
 			return false;
 		}
 	}
--- test.js
+++ test.js
@@ -169,6 +169,7 @@ test('has', t => {
 	t.is(m.has({foo: {bar: {baz: true}}}, 'foo.bar.baz'), true);
 	t.is(m.has({foo: {bar: {baz: null}}}, 'foo.bar.baz'), true);
 	t.is(m.has({foo: {bar: 'a'}}, 'foo.fake.fake2'), false);
+	t.is(m.has({foo: null}, 'foo.bar'), false);
 
 	function fn() {}
 	fn.foo = {bar: 1};
