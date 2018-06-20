fe1feb279b145470d84a77cea6f7bf2e994ca59b
Thor Sedeke <33289213+ThorSedeke@users.noreply.github.com>
2018-06-20 09:06:34 +0200

    Update benchmark (#49)

--- bench.js
+++ bench.js
@@ -1,8 +1,11 @@
 'use strict';
-/* globals bench */
+
+const Benchmark = require('benchmark');
 const m = require('.');
 
-bench('get', () => {
+const suite = new Benchmark.Suite();
+
+suite.add('get', () => {
 	const f1 = {foo: {bar: 1}};
 	m.get(f1);
 	f1[''] = 'foo';
@@ -48,112 +51,113 @@ bench('get', () => {
 	m.get('foo', 'foo.bar', false);
 	m.get([], 'foo.bar', false);
 	m.get(undefined, 'foo.bar', false);
-});
+})
+	.add('set', () => {
+		const func = () => 'test';
+		let f1 = {};
 
-bench('set', () => {
-	const func = () => 'test';
-	let f1 = {};
+		m.set(f1, 'foo', 2);
 
-	m.set(f1, 'foo', 2);
+		f1 = {foo: {bar: 1}};
+		m.set(f1, 'foo.bar', 2);
 
-	f1 = {foo: {bar: 1}};
-	m.set(f1, 'foo.bar', 2);
+		m.set(f1, 'foo.bar.baz', 3);
 
-	m.set(f1, 'foo.bar.baz', 3);
+		m.set(f1, 'foo.bar', 'test');
 
-	m.set(f1, 'foo.bar', 'test');
+		m.set(f1, 'foo.bar', null);
 
-	m.set(f1, 'foo.bar', null);
+		m.set(f1, 'foo.bar', false);
 
-	m.set(f1, 'foo.bar', false);
+		m.set(f1, 'foo.bar', undefined);
 
-	m.set(f1, 'foo.bar', undefined);
+		m.set(f1, 'foo.fake.fake2', 'fake');
 
-	m.set(f1, 'foo.fake.fake2', 'fake');
+		m.set(f1, 'foo.function', func);
 
-	m.set(f1, 'foo.function', func);
+		function fn() {}
+		m.set(fn, 'foo.bar', 1);
 
-	function fn() {}
-	m.set(fn, 'foo.bar', 1);
+		f1.fn = fn;
+		m.set(f1, 'fn.bar.baz', 2);
 
-	f1.fn = fn;
-	m.set(f1, 'fn.bar.baz', 2);
+		const f2 = {foo: null};
+		m.set(f2, 'foo.bar', 2);
 
-	const f2 = {foo: null};
-	m.set(f2, 'foo.bar', 2);
+		const f3 = {};
+		m.set(f3, '', 3);
 
-	const f3 = {};
-	m.set(f3, '', 3);
+		m.set(f1, 'foo\\.bar.baz', true);
 
-	m.set(f1, 'foo\\.bar.baz', true);
+		m.set(f1, 'fo\\.ob\\.ar.baz', true);
+	})
+	.add('delete', () => {
+		const func = () => 'test';
+		func.foo = 'bar';
 
-	m.set(f1, 'fo\\.ob\\.ar.baz', true);
-});
+		const inner = {
+			a: 'a',
+			b: 'b',
+			c: 'c',
+			func
+		};
 
-bench('delete', () => {
-	const func = () => 'test';
-	func.foo = 'bar';
-
-	const inner = {
-		a: 'a',
-		b: 'b',
-		c: 'c',
-		func
-	};
-
-	const f1 = {
-		foo: {
-			bar: {
-				baz: inner
+		const f1 = {
+			foo: {
+				bar: {
+					baz: inner
+				}
+			},
+			top: {
+				dog: 'sindre'
 			}
-		},
-		top: {
-			dog: 'sindre'
-		}
-	};
+		};
 
-	m.delete(f1, 'foo.bar.baz.c');
+		m.delete(f1, 'foo.bar.baz.c');
 
-	m.delete(f1, 'top');
+		m.delete(f1, 'top');
 
-	m.delete(f1, 'foo.bar.baz.func.foo');
+		m.delete(f1, 'foo.bar.baz.func.foo');
 
-	m.delete(f1, 'foo.bar.baz.func');
+		m.delete(f1, 'foo.bar.baz.func');
 
-	m.set(f1, 'foo\\.bar.baz', true);
-	m.delete(f1, 'foo\\.bar.baz');
+		m.set(f1, 'foo\\.bar.baz', true);
+		m.delete(f1, 'foo\\.bar.baz');
 
-	const f2 = {};
-	m.set(f2, 'foo.bar\\.baz', true);
-	m.delete(f2, 'foo.bar\\.baz');
+		const f2 = {};
+		m.set(f2, 'foo.bar\\.baz', true);
+		m.delete(f2, 'foo.bar\\.baz');
 
-	f2.dotted = {
-		sub: {
-			'dotted.prop': 'foo',
-			other: 'prop'
-		}
-	};
-	m.delete(f2, 'dotted.sub.dotted\\.prop');
-});
+		f2.dotted = {
+			sub: {
+				'dotted.prop': 'foo',
+				other: 'prop'
+			}
+		};
+		m.delete(f2, 'dotted.sub.dotted\\.prop');
+	})
+	.add('has', () => {
+		const f1 = {foo: {bar: 1}};
+		m.has(f1);
+		m.has(f1, 'foo');
+		m.has({foo: 1}, 'foo');
+		m.has({foo: null}, 'foo');
+		m.has({foo: undefined}, 'foo');
+		m.has({foo: {bar: true}}, 'foo.bar');
+		m.has({foo: {bar: {baz: true}}}, 'foo.bar.baz');
+		m.has({foo: {bar: {baz: null}}}, 'foo.bar.baz');
+		m.has({foo: {bar: 'a'}}, 'foo.fake.fake2');
 
-bench('has', () => {
-	const f1 = {foo: {bar: 1}};
-	m.has(f1);
-	m.has(f1, 'foo');
-	m.has({foo: 1}, 'foo');
-	m.has({foo: null}, 'foo');
-	m.has({foo: undefined}, 'foo');
-	m.has({foo: {bar: true}}, 'foo.bar');
-	m.has({foo: {bar: {baz: true}}}, 'foo.bar.baz');
-	m.has({foo: {bar: {baz: null}}}, 'foo.bar.baz');
-	m.has({foo: {bar: 'a'}}, 'foo.fake.fake2');
+		function fn() {}
+		fn.foo = {bar: 1};
+		m.has(fn);
+		m.has(fn, 'foo');
+		m.has(fn, 'foo.bar');
 
-	function fn() {}
-	fn.foo = {bar: 1};
-	m.has(fn);
-	m.has(fn, 'foo');
-	m.has(fn, 'foo.bar');
-
-	m.has({'foo.baz': {bar: true}}, 'foo\\.baz.bar');
-	m.has({'fo.ob.az': {bar: true}}, 'fo\\.ob\\.az.bar');
-});
+		m.has({'foo.baz': {bar: true}}, 'foo\\.baz.bar');
+		m.has({'fo.ob.az': {bar: true}}, 'fo\\.ob\\.az.bar');
+	}).on('cycle', event => {
+		console.log(String(event.target));
+	}).on('complete', () => {
+		console.log('Finished');
+	}).run({async: true});
