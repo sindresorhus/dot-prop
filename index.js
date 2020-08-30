ca9296aa95aab0d39dc798146b85122687146b82
Richie Bendall <richiebendall@gmail.com>
2020-08-30 22:44:15 +1200

    Assign exported functions to `exports`
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- index.js
+++ index.js
@@ -31,112 +31,110 @@ function getPathSegments(path) {
 	return parts;
 }
 
-module.exports = {
-	get(object, path, value) {
-		if (!isObj(object) || typeof path !== 'string') {
-			return value === undefined ? object : value;
+exports.get = (object, path, value) => {
+	if (!isObj(object) || typeof path !== 'string') {
+		return value === undefined ? object : value;
+	}
+
+	const pathArray = getPathSegments(path);
+	if (pathArray.length === 0) {
+		return;
+	}
+
+	for (let i = 0; i < pathArray.length; i++) {
+		if (!Object.prototype.propertyIsEnumerable.call(object, pathArray[i])) {
+			return value;
 		}
 
-		const pathArray = getPathSegments(path);
-		if (pathArray.length === 0) {
-			return;
-		}
+		object = object[pathArray[i]];
 
-		for (let i = 0; i < pathArray.length; i++) {
-			if (!Object.prototype.propertyIsEnumerable.call(object, pathArray[i])) {
+		if (object === undefined || object === null) {
+			// `object` is either `undefined` or `null` so we want to stop the loop, and
+			// if this is not the last bit of the path, and
+			// if it did't return `undefined`
+			// it would return `null` if `object` is `null`
+			// but we want `get({foo: null}, 'foo.bar')` to equal `undefined`, or the supplied value, not `null`
+			if (i !== pathArray.length - 1) {
 				return value;
 			}
 
-			object = object[pathArray[i]];
-
-			if (object === undefined || object === null) {
-				// `object` is either `undefined` or `null` so we want to stop the loop, and
-				// if this is not the last bit of the path, and
-				// if it did't return `undefined`
-				// it would return `null` if `object` is `null`
-				// but we want `get({foo: null}, 'foo.bar')` to equal `undefined`, or the supplied value, not `null`
-				if (i !== pathArray.length - 1) {
-					return value;
-				}
-
-				break;
-			}
+			break;
 		}
+	}
 
+	return object;
+};
+
+exports.set = (object, path, value) => {
+	if (!isObj(object) || typeof path !== 'string') {
 		return object;
-	},
+	}
 
-	set(object, path, value) {
-		if (!isObj(object) || typeof path !== 'string') {
-			return object;
+	const root = object;
+	const pathArray = getPathSegments(path);
+
+	for (let i = 0; i < pathArray.length; i++) {
+		const p = pathArray[i];
+
+		if (!isObj(object[p])) {
+			object[p] = {};
 		}
 
-		const root = object;
-		const pathArray = getPathSegments(path);
-
-		for (let i = 0; i < pathArray.length; i++) {
-			const p = pathArray[i];
-
-			if (!isObj(object[p])) {
-				object[p] = {};
-			}
-
-			if (i === pathArray.length - 1) {
-				object[p] = value;
-			}
-
-			object = object[p];
+		if (i === pathArray.length - 1) {
+			object[p] = value;
 		}
 
-		return root;
-	},
+		object = object[p];
+	}
 
-	delete(object, path) {
-		if (!isObj(object) || typeof path !== 'string') {
+	return root;
+};
+
+exports.delete = (object, path) => {
+	if (!isObj(object) || typeof path !== 'string') {
+		return;
+	}
+
+	const pathArray = getPathSegments(path);
+
+	for (let i = 0; i < pathArray.length; i++) {
+		const p = pathArray[i];
+
+		if (i === pathArray.length - 1) {
+			delete object[p];
 			return;
 		}
 
-		const pathArray = getPathSegments(path);
+		object = object[p];
 
-		for (let i = 0; i < pathArray.length; i++) {
-			const p = pathArray[i];
-
-			if (i === pathArray.length - 1) {
-				delete object[p];
-				return;
-			}
-
-			object = object[p];
-
-			if (!isObj(object)) {
-				return;
-			}
+		if (!isObj(object)) {
+			return;
 		}
-	},
+	}
+};
 
-	has(object, path) {
-		if (!isObj(object) || typeof path !== 'string') {
-			return false;
-		}
+exports.has = (object, path) => {
+	if (!isObj(object) || typeof path !== 'string') {
+		return false;
+	}
 
-		const pathArray = getPathSegments(path);
-		if (pathArray.length === 0) {
-			return false;
-		}
+	const pathArray = getPathSegments(path);
+	if (pathArray.length === 0) {
+		return false;
+	}
 
-		// eslint-disable-next-line unicorn/no-for-loop
-		for (let i = 0; i < pathArray.length; i++) {
-			if (isObj(object)) {
-				if (!(pathArray[i] in object)) {
-					return false;
-				}
-
-				object = object[pathArray[i]];
-			} else {
+	// eslint-disable-next-line unicorn/no-for-loop
+	for (let i = 0; i < pathArray.length; i++) {
+		if (isObj(object)) {
+			if (!(pathArray[i] in object)) {
 				return false;
 			}
-		}
 
-		return true;
+			object = object[pathArray[i]];
+		} else {
+			return false;
+		}
 	}
+
+	return true;
 };
